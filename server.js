120c70060a7b41ddf8b937056b3c2af9220a08d5
Claudio Gallo <cgb1408@outlook.com>
2020-09-29 19:50:18 -0600

    Created and edited server.js file

---
+++ server.js
@@ -0,0 +1,34 @@
+//REQUIRING npm packages
+const express = require("express");
+const session = require("express-session");
+//Passport required as we've configured it
+const passport = require("./config/passport");
+//Setting up PORT
+const PORT = process.env.PORT || 8080;
+const db = require("./models");
+//Creating express app and configuring middleware
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+//Session keeps track of our user's login status
+app.use(
+    session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+//Syncing our database
+db.sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+        console.log(
+            'Listening on port: ' + PORT,
+            PORT,
+            PORT
+        );
+    });
+});
