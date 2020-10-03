cec920a92a917c24befaebfeee1b8f39e7030d22
keldykins <kelcundy@gmail.com>
2020-10-03 11:48:22 -0600

    saving work

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const session = require("express-session");
 const passport = require("./config/passport");
 //Setting up PORT
 const PORT = process.env.PORT || 8080;
+
 const db = require("./models");
 //Creating express app and configuring middleware
 const app = express();
@@ -14,8 +15,20 @@ app.use(express.json());
 app.use(express.static("public"));
 //Session keeps track of our user's login status
 app.use(
-    session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
 );
+//Sets handlebars configurations (we will go through them later on)
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+  })
+);
+app.get("/", (req, res) => {
+  //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+  res.render("index");
+});
+app.set("view engine", "handlebars");
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -23,16 +36,12 @@ app.use(passport.session());
 require("./config/connection");
 
 //Requiring our routes
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+// require("./routes/html-routes.js")(app);
+// require("./routes/api-routes.js")(app);
 
 //Syncing our database
 db.sequelize.sync().then(() => {
-    app.listen(PORT, () => {
-        console.log(
-            'Listening on port: ' + PORT,
-            PORT,
-            PORT
-        );
-    });
+  app.listen(PORT, () => {
+    console.log("Listening on port: " + PORT, PORT, PORT);
+  });
 });
