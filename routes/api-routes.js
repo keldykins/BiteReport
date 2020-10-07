0e7bb2a266a34e1d40cea58340acf64a99c7f2a3
Jake Haberle <jake.haberle@gmail.com>
2020-10-06 19:30:36 -0600

    added post and get routes to api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -40,3 +40,41 @@ module.exports = function(app) {
 
     
 };
+
+module.exports = function(app) {
+
+    // GET route for getting all of the posts
+    app.get("/api/reviews", function(req, res) {
+      var query = {};
+    //   if (req.query.id) {
+    //     query.AuthorId = req.query.author_id;
+    //   }
+      db.Post.findAll({
+        where: query
+      }).then(function(dbPost) {
+        res.json(dbPost);
+      });
+    });
+  
+
+  
+    // POST route for saving a new post
+    app.post("/api/reviews", function(req, res) {
+      db.Post.create(req.body).then(function(dbPost) {
+        res.json(dbPost);
+      });
+    });
+  
+    // DELETE route for deleting posts
+    // app.delete("/api/posts/:id", function(req, res) {
+    //   db.Post.destroy({
+    //     where: {
+    //       id: req.params.id
+    //     }
+    //   }).then(function(dbPost) {
+    //     res.json(dbPost);
+    //   });
+    // });
+  
+    
+  };
