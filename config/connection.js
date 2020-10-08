ac2711576ed3cf4b0354d5d89aa31580427b5752
gbluemd <gsjujub@gmail.com>
2020-10-08 14:52:21 -0600

    css & index file modifications

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("rest_reviewsdb", "root", "M@rif3fa", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
+const sequelize = new Sequelize("rest_reviewsdb", "root", "", {
+    host: "localhost",
+    port: 3306,
+    dialect: "mysql",
 });
+
 
 module.exports = sequelize;
